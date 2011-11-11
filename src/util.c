xfce-mirror/ristretto

commit c3bb5cc57e77fcd46fd18d1d17a914d584e3edd8

Fix bug #8134 (Fix compile with --enable-debug=full)
- Patch slightly modified by Stephan Arts <stephan@xfce.org>
  ( Notebook is allowed to be removed )

src/util.c
@@ -18,11 +18,12 @@
 
 #include <config.h>
 #include <glib.h>
-
+#include <locale.h>
 #include <exo/exo.h>
 
+void rstto_launch_help (void);
 
-gboolean
+void
 rstto_launch_help (void)
 {
     gchar *locale = NULL;

src/properties_dialog.c
@@ -59,7 +59,7 @@ properties_dialog_set_file (
 
 static GtkWidgetClass *parent_class = NULL;
 
-static enum
+enum
 {
     EXIF_PROP_DATE_TIME = 0,
     EXIF_PROP_MODEL,
@@ -125,7 +125,6 @@ static void
 rstto_properties_dialog_init (RsttoPropertiesDialog *dialog)
 {
     GtkWidget *vbox;
-    GtkWidget *notebook;
     GtkWidget *table;
     /* General tab */
     GtkWidget *general_label;

src/xfce_wallpaper_manager.c
@@ -119,12 +119,15 @@ rstto_xfce_wallpaper_manager_configure_dialog_run (
         RsttoWallpaperManager *self,
         RsttoFile *file)
 {
+    GdkPixbuf *monitor_pixbuf;
+    gdouble saturation;
+    gdouble brightness;
     RsttoXfceWallpaperManager *manager = RSTTO_XFCE_WALLPAPER_MANAGER (self);
     gint response = 0;
     manager->priv->file = file;
-    GdkPixbuf *monitor_pixbuf = NULL;
-    gdouble saturation = gtk_adjustment_get_value (GTK_ADJUSTMENT(manager->priv->saturation_adjustment));
-    gdouble brightness = gtk_adjustment_get_value (GTK_ADJUSTMENT(manager->priv->brightness_adjustment));
+    monitor_pixbuf = NULL;
+    saturation = gtk_adjustment_get_value (GTK_ADJUSTMENT(manager->priv->saturation_adjustment));
+    brightness = gtk_adjustment_get_value (GTK_ADJUSTMENT(manager->priv->brightness_adjustment));
 
     if (manager->priv->pixbuf)
     {

commit 694b846b7502a398a4b2a61d5209925ae7eb0de8

Use the correct return-type for rstto_launch_help.
- Fix bug introduced in commit
  c3bb5cc57e77fcd46fd18d1d17a914d584e3edd8

src/util.c
@@ -21,9 +21,9 @@
 #include <locale.h>
 #include <exo/exo.h>
 
-void rstto_launch_help (void);
+#include "util.h"
 
-void
+gboolean
 rstto_launch_help (void)
 {
     gchar *locale = NULL;
@@ -103,4 +103,6 @@ rstto_launch_help (void)
 
     g_free (docpath);
     g_free (cur_dir);
+
+    return TRUE;
 }