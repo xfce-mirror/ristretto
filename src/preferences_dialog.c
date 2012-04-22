2e5441ed6bdcd7e79c2719694cdda33cf3fc48d3
Stephan Arts <stephan@xfce.org>
2012-04-22 19:04:38 +0200

    Add description-text to the preferences-dialog

--- src/preferences_dialog.c
+++ src/preferences_dialog.c
@@ -100,6 +100,7 @@ struct _RsttoPreferencesDialogPriv
 
         GtkWidget *clock_vbox;
         GtkWidget *clock_frame;
+        GtkWidget *clock_label;
         GtkWidget *clock_button;
     } slideshow_tab;
 
@@ -220,7 +221,8 @@ rstto_preferences_dialog_init(RsttoPreferencesDialog *dialog)
     dialog->priv->display_tab.thumbnail_frame = xfce_gtk_frame_box_new_with_content(_("Thumbnails"), dialog->priv->display_tab.thumbnail_vbox);
     gtk_box_pack_start (GTK_BOX (display_main_vbox), dialog->priv->display_tab.thumbnail_frame, FALSE, FALSE, 0);
 
-    dialog->priv->display_tab.hide_thumbnails_fullscreen_lbl = gtk_label_new(_("The thumbnail bar can be automatically hidden \nwhen the window is fullscreen."));
+    dialog->priv->display_tab.hide_thumbnails_fullscreen_lbl = gtk_label_new(_("The thumbnail bar can be automatically hidden when the window is fullscreen."));
+    gtk_label_set_line_wrap (GTK_LABEL (dialog->priv->display_tab.hide_thumbnails_fullscreen_lbl), TRUE);
     gtk_misc_set_alignment(GTK_MISC(dialog->priv->display_tab.hide_thumbnails_fullscreen_lbl), 0, 0.5);
     dialog->priv->display_tab.hide_thumbnails_fullscreen_check_button = gtk_check_button_new_with_label (_("Hide thumbnail bar when fullscreen"));
     gtk_box_pack_start (GTK_BOX (dialog->priv->display_tab.thumbnail_vbox), dialog->priv->display_tab.hide_thumbnails_fullscreen_lbl, FALSE, FALSE, 0);
@@ -272,7 +274,11 @@ rstto_preferences_dialog_init(RsttoPreferencesDialog *dialog)
     dialog->priv->slideshow_tab.clock_frame = xfce_gtk_frame_box_new_with_content(_("Clock"), dialog->priv->slideshow_tab.clock_vbox);
     gtk_box_pack_start (GTK_BOX (slideshow_main_vbox), dialog->priv->slideshow_tab.clock_frame, FALSE, FALSE, 0);
 
+    dialog->priv->slideshow_tab.clock_label = gtk_label_new ( _("Show an analog clock that displays the current time when the window is fullscreen"));
+    gtk_label_set_line_wrap (GTK_LABEL (dialog->priv->slideshow_tab.clock_label), TRUE);
+    gtk_misc_set_alignment(GTK_MISC(dialog->priv->slideshow_tab.clock_label), 0, 0.5);
     dialog->priv->slideshow_tab.clock_button = gtk_check_button_new_with_label (_("Show Fullscreen Clock"));
+    gtk_container_add (GTK_CONTAINER (dialog->priv->slideshow_tab.clock_vbox), dialog->priv->slideshow_tab.clock_label);
     gtk_container_add (GTK_CONTAINER (dialog->priv->slideshow_tab.clock_vbox), dialog->priv->slideshow_tab.clock_button);
     gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (dialog->priv->slideshow_tab.clock_button), bool_show_clock);
     
