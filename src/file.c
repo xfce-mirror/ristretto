f5440e7eb6afe814b0868842c575f0152a6ecec3
Igor <f2404@yandex.ru>
2016-10-11 16:53:33 +0300

    Fall back to GFileInfo method in case of libmagic failure
    Also do not query file content type multiple times

--- src/file.c
+++ src/file.c
@@ -23,12 +23,12 @@
 
 #include <libxfce4util/libxfce4util.h>
 
-#include "file.h"
-#include "thumbnailer.h"
-
 #if HAVE_MAGIC_H
 #include <magic.h>
 #endif
+
+#include "file.h"
+#include "thumbnailer.h"
 
 static guint rstto_thumbnail_size[] =
 {
@@ -398,54 +398,45 @@ rstto_file_get_collate_key ( RsttoFile *r_file )
 const gchar *
 rstto_file_get_content_type ( RsttoFile *r_file )
 {
-    const gchar *content_type;
-
-#if HAVE_MAGIC_H
-    magic_t magic = NULL;
-
-    if ((magic = magic_open(MAGIC_MIME_TYPE)) == NULL)
-    {
-        fprintf(stderr, "unable to initialize magic library\n");
-        return NULL;
-    }
-
-    if (magic_load(magic, NULL) != 0)
-    {
-        fprintf(stderr, "cannot load magic database: %s\n", magic_error(magic));
-        magic_close(magic);
-        return NULL;
-    }
-
-    content_type = magic_file(magic, rstto_file_get_path(r_file));
-
-    if (NULL != content_type)
-    {
-        r_file->priv->content_type = g_strdup (content_type);
-    }
-
-    magic_close(magic);
-#else
-    GFileInfo *file_info = NULL;
+    const gchar *content_type = NULL;
 
     if ( NULL == r_file->priv->content_type )
     {
-        file_info = g_file_query_info (
-                r_file->priv->file,
-                "standard::content-type",
-                0,
-                NULL,
-                NULL );
-        if ( NULL != file_info )
+#if HAVE_MAGIC_H
+        magic_t magic = magic_open(MAGIC_MIME_TYPE);
+        if ( magic != NULL )
         {
-            content_type = g_file_info_get_content_type (file_info);
-            if ( NULL != content_type )
+            if ( magic_load(magic, NULL) == 0 )
             {
-                r_file->priv->content_type = g_strdup (content_type);
+                content_type = magic_file(magic, rstto_file_get_path(r_file));
+                if ( NULL != content_type )
+                {
+                    r_file->priv->content_type = g_strdup (content_type);
+                }
             }
-            g_object_unref (file_info);
+            magic_close(magic);
+        }
+#endif
+
+        if ( NULL == content_type )
+        {
+            GFileInfo *file_info = g_file_query_info (
+                    r_file->priv->file,
+                    "standard::content-type",
+                    0,
+                    NULL,
+                    NULL );
+            if ( NULL != file_info )
+            {
+                content_type = g_file_info_get_content_type (file_info);
+                if ( NULL != content_type )
+                {
+                    r_file->priv->content_type = g_strdup (content_type);
+                }
+                g_object_unref (file_info);
+            }
         }
     }
-#endif
 
     return (const gchar *)r_file->priv->content_type;
 }
