xfce-mirror/ristretto

commit d5e4fa6d8172c7b8b9bbf917a2f74bef41394a52

Cleanup: Pass orientation directly from file to viewer
See !28 for more details.

src/file.c
@@ -87,6 +87,7 @@ static void
 rstto_file_init (RsttoFile *r_file)
 {
     r_file->priv = rstto_file_get_instance_private (r_file);
+    r_file->priv->orientation = RSTTO_IMAGE_ORIENT_NONE;
 }
 
 

src/image_viewer.c
@@ -214,7 +214,6 @@ struct _RsttoImageViewerTransaction
     gint              image_height;
     gdouble           image_scale;
     gdouble           scale;
-    RsttoImageOrientation orientation;
 
     /* File I/O data */
     /*****************/
@@ -1375,11 +1374,10 @@ void
 rstto_image_viewer_set_orientation (RsttoImageViewer *viewer, RsttoImageOrientation orientation)
 {
     viewer->priv->orientation = orientation;
+    rstto_file_set_orientation (viewer->priv->file, orientation);
 
     if (viewer->priv->auto_scale)
         set_scale (viewer, RSTTO_SCALE_FIT_TO_VIEW);
-
-    rstto_file_set_orientation (viewer->priv->file, orientation);
 
     set_adjustments (viewer,
                      gtk_adjustment_get_value (viewer->priv->hadjustment),
@@ -1614,8 +1612,6 @@ cb_rstto_image_loader_size_prepared (GdkPixbufLoader *loader, gint width, gint h
             transaction->image_scale = 1.0;
         }
     }
-
-    transaction->orientation = rstto_file_get_orientation (transaction->file);
 }
 
 static gboolean
@@ -1635,7 +1631,6 @@ cb_rstto_image_loader_closed_idle (gpointer data)
             viewer->priv->image_scale = transaction->image_scale;
             viewer->priv->image_width = transaction->image_width;
             viewer->priv->image_height = transaction->image_height;
-            viewer->priv->orientation = transaction->orientation;
             set_scale (viewer, transaction->scale);
 
             cb_rstto_image_loader_image_ready (transaction->loader, transaction);

src/main_window.c
@@ -1400,7 +1400,8 @@ G_GNUC_END_IGNORE_DEPRECATIONS
             content_type = rstto_file_get_content_type (cur_file);
 
             rstto_image_viewer_set_file (RSTTO_IMAGE_VIEWER (window->priv->image_viewer),
-                                         cur_file, RSTTO_SCALE_IMAGE_LOADING, 0);
+                                         cur_file, RSTTO_SCALE_IMAGE_LOADING,
+                                         rstto_file_get_orientation (cur_file));
 
             pixbuf = rstto_file_get_thumbnail (cur_file, THUMBNAIL_SIZE_SMALL);
             if (pixbuf != NULL)
@@ -1495,7 +1496,7 @@ G_GNUC_END_IGNORE_DEPRECATIONS
             gtk_widget_set_sensitive (menu_item, FALSE);
 
             rstto_image_viewer_set_file (RSTTO_IMAGE_VIEWER (window->priv->image_viewer),
-                                         NULL, RSTTO_SCALE_IMAGE_LOADING, 0);
+                                         NULL, RSTTO_SCALE_IMAGE_LOADING, RSTTO_IMAGE_ORIENT_NONE);
 
             menu_item = gtk_menu_item_new_with_label (_("Empty"));
             gtk_menu_shell_append (GTK_MENU_SHELL (open_with_window_menu), menu_item);

commit 355d15d98cd3faa19df9db5139088035df5cfde9

Fix regression: Exif orientation is not respected
Introduced in d5e4fa6d8172c7b8b9bbf917a2f74bef41394a52.

Fixes #69, related to !28.

src/file.c
@@ -84,7 +84,7 @@ static void
 rstto_file_init (RsttoFile *r_file)
 {
     r_file->priv = rstto_file_get_instance_private (r_file);
-    r_file->priv->orientation = RSTTO_IMAGE_ORIENT_NONE;
+    r_file->priv->orientation = RSTTO_IMAGE_ORIENT_NOT_DETERMINED;
     r_file->priv->scale = RSTTO_SCALE_NONE;
     r_file->priv->auto_scale = RSTTO_SCALE_NONE;
 }
@@ -408,7 +408,7 @@ RsttoImageOrientation
 rstto_file_get_orientation (RsttoFile *r_file)
 {
     ExifEntry *exif_entry = NULL;
-    if (r_file->priv->orientation == 0)
+    if (r_file->priv->orientation == RSTTO_IMAGE_ORIENT_NOT_DETERMINED)
     {
         /* Try to get the default orientation from the EXIF tag */
         exif_entry = rstto_file_get_exif (r_file, EXIF_TAG_ORIENTATION);
@@ -420,7 +420,7 @@ rstto_file_get_orientation (RsttoFile *r_file)
         }
 
         /* If the orientation-tag is not set, default to NONE */
-        if (r_file->priv->orientation == 0)
+        if (r_file->priv->orientation == RSTTO_IMAGE_ORIENT_NOT_DETERMINED)
         {
             /* Default orientation */
             r_file->priv->orientation = RSTTO_IMAGE_ORIENT_NONE;