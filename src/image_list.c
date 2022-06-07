xfce-mirror/ristretto

commit d135e3ad185c90cf3527b786b856e1bfffe30cfc

Cleanup: Miscellaneous
* RsttoFile: Remove `rstto_file_equal()` which does nothing but hide a
  pointer equality.
* RsttoImageList: Simplify `remove_file()` and keep sticky state.
* RsttoImageViewer: Do not emit "size-ready" for a cancelled
  transaction.
* RsttoMainWindow: Set window icon in one place.
* RsttoMainWindow: Rename `update_title()` to `set_title()` for
  consistency.

src/image_list.c
@@ -396,75 +396,47 @@ rstto_image_list_get_iter (RsttoImageList *image_list)
 
 
 void
-rstto_image_list_remove_file (
-        RsttoImageList *image_list,
-        RsttoFile *r_file)
+rstto_image_list_remove_file (RsttoImageList *image_list,
+                              RsttoFile *r_file)
 {
-    GSList *iter = NULL;
-    RsttoFile *r_file_a = NULL;
-    GtkTreePath *path_ = NULL;
-    gint index_ = g_list_index (image_list->priv->images, r_file);
-    gint n_images = rstto_image_list_get_n_images (image_list);
+    RsttoImageListIter *r_iter;
+    GSList *iter;
+    GtkTreePath *path_;
+    gint index_;
 
-    if (index_ != -1)
+    index_ = g_list_index (image_list->priv->images, r_file);
+    if (index_ == -1)
+        return;
+
+    for (iter = image_list->priv->iterators; iter != NULL; iter = iter->next)
     {
+        r_iter = iter->data;
+        if (r_iter->priv->r_file != r_file)
+            continue;
 
-        iter = image_list->priv->iterators;
-        while (iter)
+        if (r_iter->priv->r_file == g_list_last (image_list->priv->images)->data)
+            iter_previous (r_iter, r_iter->priv->sticky);
+        else
+            iter_next (r_iter, r_iter->priv->sticky);
+
+        /* if the image is still the same, it's a single item list,
+         * and we should force the image in this iter to NULL */
+        if (r_iter->priv->r_file == r_file)
         {
-            if (rstto_file_equal (rstto_image_list_iter_get_file (iter->data), r_file))
-            {
-                if (rstto_image_list_iter_get_position (iter->data) == n_images - 1)
-                {
-                    iter_previous (iter->data, FALSE);
-                }
-                else
-                {
-                    iter_next (iter->data, FALSE);
-                }
-                /* If the image is still the same,
-                 * it's a single item list,
-                 * and we should force the image in this iter to NULL
-                 */
-                if (rstto_file_equal (
-                        rstto_image_list_iter_get_file (iter->data),
-                        r_file))
-                {
-
-                    image_list->priv->images = g_list_remove (image_list->priv->images, r_file);
-                    RSTTO_IMAGE_LIST_ITER (iter->data)->priv->r_file = NULL;
-                    g_signal_emit (iter->data,
-                            rstto_image_list_iter_signals[RSTTO_IMAGE_LIST_ITER_SIGNAL_CHANGED],
-                            0, NULL);
-                }
-            }
-            iter = g_slist_next (iter);
+            r_iter->priv->r_file = NULL;
+            g_signal_emit (r_iter,
+                           rstto_image_list_iter_signals[RSTTO_IMAGE_LIST_ITER_SIGNAL_CHANGED],
+                           0, NULL);
         }
-
-        image_list->priv->images = g_list_remove (image_list->priv->images, r_file);
-
-        path_ = gtk_tree_path_new ();
-        gtk_tree_path_append_index (path_, index_);
-
-        gtk_tree_model_row_deleted (GTK_TREE_MODEL (image_list), path_);
-
-        iter = image_list->priv->iterators;
-        while (iter)
-        {
-            r_file_a = rstto_image_list_iter_get_file (iter->data);
-            if (NULL != r_file_a)
-            {
-                if (rstto_file_equal (r_file_a, r_file))
-                {
-                    iter_next (iter->data, FALSE);
-                }
-            }
-            iter = g_slist_next (iter);
-        }
-
-        g_object_unref (r_file);
-        gtk_tree_path_free (path_);
     }
+
+    image_list->priv->images = g_list_remove (image_list->priv->images, r_file);
+    g_object_unref (r_file);
+
+    path_ = gtk_tree_path_new ();
+    gtk_tree_path_append_index (path_, index_);
+    gtk_tree_model_row_deleted (GTK_TREE_MODEL (image_list), path_);
+    gtk_tree_path_free (path_);
 }
 
 static void

src/file.c
@@ -287,12 +287,6 @@ rstto_file_get_file (RsttoFile *r_file)
     return r_file->priv->file;
 }
 
-gboolean
-rstto_file_equal (RsttoFile *r_file_a, RsttoFile *r_file_b)
-{
-    return r_file_a == r_file_b;
-}
-
 const gchar *
 rstto_file_get_display_name (RsttoFile *r_file)
 {

src/file.h
@@ -98,10 +98,6 @@ rstto_file_is_valid (RsttoFile *r_file);
 GFile *
 rstto_file_get_file (RsttoFile *r_file);
 
-gboolean
-rstto_file_equal (RsttoFile *r_file_a,
-                  RsttoFile *r_file_b);
-
 const gchar *
 rstto_file_get_display_name (RsttoFile *r_file);
 

src/image_viewer.c
@@ -1264,7 +1264,7 @@ rstto_image_viewer_set_file (RsttoImageViewer *viewer,
             /*
              * If the old, and new file are equal, do nothing.
              */
-            if (!rstto_file_equal (viewer->priv->file, file))
+            if (viewer->priv->file != file)
             {
                 /*
                  * This will first need to return to the 'main' loop before it cleans up after itself.
@@ -1778,9 +1778,9 @@ cb_rstto_image_loader_closed_idle (gpointer data)
                          gtk_adjustment_get_value (viewer->priv->hadjustment),
                          gtk_adjustment_get_value (viewer->priv->vadjustment));
         gdk_window_invalidate_rect (gtk_widget_get_window (widget), NULL, FALSE);
-    }
 
-    g_signal_emit_by_name (transaction->viewer, "size-ready");
+        g_signal_emit_by_name (transaction->viewer, "size-ready");
+    }
 
     transaction->loader_closed_id = 0;
 

src/main_window.c
@@ -103,10 +103,6 @@ static gint
 cb_compare_app_infos (gconstpointer a,
                       gconstpointer b);
 static void
-cb_rstto_thumbnailer_ready (RsttoThumbnailer *thumbnailer,
-                            RsttoFile *file,
-                            gpointer user_data);
-static void
 rstto_main_window_image_list_iter_changed (RsttoMainWindow *window);
 static gboolean
 rstto_main_window_recent_filter (const GtkRecentFilterInfo *filter_info,
@@ -214,7 +210,10 @@ cb_rstto_main_window_dnd_files (GtkWidget *widget,
                                 gchar **uris,
                                 RsttoMainWindow *window);
 static void
-cb_rstto_main_window_update_title (RsttoMainWindow *window);
+cb_rstto_main_window_set_title (RsttoMainWindow *window);
+static void
+cb_rstto_main_window_set_icon (RsttoMainWindow *window,
+                               RsttoFile *file);
 
 static void
 cb_rstto_main_window_set_as_wallpaper (GtkWidget *widget,
@@ -1278,8 +1277,8 @@ G_GNUC_END_IGNORE_DEPRECATIONS
     g_signal_connect (window->priv->settings_manager, "notify::desktop-type",
                       G_CALLBACK (cb_rstto_desktop_type_changed), window);
 
-    g_signal_connect (window->priv->thumbnailer, "ready",
-                      G_CALLBACK (cb_rstto_thumbnailer_ready), window);
+    g_signal_connect_swapped (window->priv->thumbnailer, "ready",
+                              G_CALLBACK (cb_rstto_main_window_set_icon), window);
 }
 
 static void
@@ -1501,9 +1500,9 @@ rstto_main_window_new (RsttoImageList *image_list, gboolean fullscreen)
 
     app_image_list = window->priv->image_list = g_object_ref (image_list);
     g_signal_connect_swapped (image_list, "row-deleted",
-                              G_CALLBACK (cb_rstto_main_window_update_title), window);
+                              G_CALLBACK (cb_rstto_main_window_set_title), window);
     g_signal_connect_swapped (image_list, "row-inserted",
-                              G_CALLBACK (cb_rstto_main_window_update_title), window);
+                              G_CALLBACK (cb_rstto_main_window_set_title), window);
 
     switch (rstto_settings_get_uint_property (window->priv->settings_manager, "sort-type"))
     {
@@ -1553,8 +1552,6 @@ rstto_main_window_image_list_iter_changed (RsttoMainWindow *window)
     const gchar     *id;
     GtkWidget       *menu_item = NULL;
     GDesktopAppInfo *app_info = NULL;
-    const GdkPixbuf *pixbuf = NULL;
-    GdkPixbuf       *tmp;
 
     GtkWidget *open_with_menu = gtk_menu_new ();
     GtkWidget *open_with_window_menu = gtk_menu_new ();
@@ -1565,9 +1562,10 @@ G_GNUC_END_IGNORE_DEPRECATIONS
 
     if (window->priv->image_list)
     {
-        cb_rstto_main_window_update_title (window);
+        cb_rstto_main_window_set_title (window);
 
         cur_file = rstto_image_list_iter_get_file (window->priv->iter);
+        cb_rstto_main_window_set_icon (window, cur_file);
         if (NULL != cur_file)
         {
             rstto_icon_bar_set_active (RSTTO_ICON_BAR (window->priv->thumbnailbar),
@@ -1580,18 +1578,6 @@ G_GNUC_END_IGNORE_DEPRECATIONS
                                          rstto_file_get_auto_scale (cur_file),
                                          rstto_file_get_orientation (cur_file));
 
-            pixbuf = rstto_file_get_thumbnail (cur_file, THUMBNAIL_SIZE_SMALL);
-            if (pixbuf != NULL)
-            {
-                tmp = gdk_pixbuf_copy (pixbuf);
-                gtk_window_set_icon (GTK_WINDOW (window), tmp);
-                g_object_unref (tmp);
-            }
-            else
-            {
-                gtk_window_set_icon (GTK_WINDOW (window), NULL);
-                gtk_window_set_icon_name (GTK_WINDOW (window), RISTRETTO_APP_ID);
-            }
 
             app_list = g_app_info_get_all_for_type (content_type);
             editor = rstto_mime_db_lookup (window->priv->db, content_type);
@@ -1670,9 +1656,6 @@ G_GNUC_END_IGNORE_DEPRECATIONS
 
             gtk_widget_show_all (open_with_menu);
             gtk_widget_show_all (open_with_window_menu);
-
-            gtk_window_set_icon (GTK_WINDOW (window), NULL);
-            gtk_window_set_icon_name (GTK_WINDOW (window), RISTRETTO_APP_ID);
         }
 
         rstto_main_window_update_buttons (window);
@@ -3928,7 +3911,7 @@ cb_rstto_main_window_dnd_files (GtkWidget *widget,
 }
 
 static void
-cb_rstto_main_window_update_title (RsttoMainWindow *window)
+cb_rstto_main_window_set_title (RsttoMainWindow *window)
 {
     RsttoFile *file;
     gchar *title;
@@ -3953,6 +3936,24 @@ cb_rstto_main_window_update_title (RsttoMainWindow *window)
 
     gtk_window_set_title (GTK_WINDOW (window), title);
     g_free (title);
+}
+
+static void
+cb_rstto_main_window_set_icon (RsttoMainWindow *window,
+                               RsttoFile *file)
+{
+    const GdkPixbuf *pixbuf;
+
+    if (file != rstto_image_list_iter_get_file (window->priv->iter))
+        return;
+
+    if (file == NULL || (pixbuf = rstto_file_get_thumbnail (file, THUMBNAIL_SIZE_SMALL)) == NULL)
+    {
+        gtk_window_set_icon (GTK_WINDOW (window), NULL);
+        gtk_window_set_icon_name (GTK_WINDOW (window), RISTRETTO_APP_ID);
+    }
+    else
+        gtk_window_set_icon (GTK_WINDOW (window), (GdkPixbuf *) pixbuf);
 }
 
 /**********************/
@@ -4704,32 +4705,4 @@ cb_compare_app_infos (
         gconstpointer b)
 {
     return g_app_info_equal (G_APP_INFO (a), G_APP_INFO (b)) ? 0 : 1;
-}
-
-static void
-cb_rstto_thumbnailer_ready (
-        RsttoThumbnailer *thumbnailer,
-        RsttoFile *file,
-        gpointer user_data)
-{
-    RsttoMainWindow *window = user_data;
-    RsttoFile *cur_file = rstto_image_list_iter_get_file (window->priv->iter);
-    const GdkPixbuf *pixbuf = NULL;
-    GdkPixbuf *tmp;
-
-    if (file == cur_file)
-    {
-        pixbuf = rstto_file_get_thumbnail (file, THUMBNAIL_SIZE_SMALL);
-        if (pixbuf != NULL)
-        {
-            tmp = gdk_pixbuf_copy (pixbuf);
-            gtk_window_set_icon (GTK_WINDOW (window), tmp);
-            g_object_unref (tmp);
-        }
-        else
-        {
-            gtk_window_set_icon (GTK_WINDOW (window), NULL);
-            gtk_window_set_icon_name (GTK_WINDOW (window), RISTRETTO_APP_ID);
-        }
-    }
 }

commit 37cb321608508cd0d556f409d8da9897edc3232a

Correctly handle deletion when there is only one image
A double regression, introduced in:
d135e3ad185c90cf3527b786b856e1bfffe30cfc
a849a32050cd5b484da71cc515b70386f46d1f9b

The first one causes a crash (division by zero), the second one causes
critical warnings.

Fixes: #89

src/image_list.c
@@ -436,22 +436,18 @@ rstto_image_list_remove_file (RsttoImageList *image_list,
             iter_previous (r_iter, r_iter->priv->sticky);
         else
             iter_next (r_iter, r_iter->priv->sticky);
-
-        /* if the image is still the same, it's a single item list,
-         * and we should force the image in this iter to NULL */
-        if (r_iter->priv->r_file == r_file)
-        {
-            r_iter->priv->r_file = NULL;
-            r_iter->priv->link = NULL;
-            r_iter->priv->index = -1;
-            g_signal_emit (r_iter,
-                           rstto_image_list_iter_signals[RSTTO_IMAGE_LIST_ITER_SIGNAL_CHANGED],
-                           0, NULL);
-        }
     }
 
-    g_queue_remove (image_list->priv->images, r_file);
-    g_object_unref (r_file);
+    if (image_list->priv->images->length == 1)
+    {
+        rstto_image_list_remove_all (image_list);
+        return;
+    }
+    else
+    {
+        g_queue_remove (image_list->priv->images, r_file);
+        g_object_unref (r_file);
+    }
 
     path_ = gtk_tree_path_new ();
     gtk_tree_path_append_index (path_, index_);
@@ -972,18 +968,19 @@ iter_next (
     {
         if (iter->priv->link->next != NULL)
         {
+            ret_val = TRUE;
             iter->priv->link = iter->priv->link->next;
             iter->priv->index++;
         }
         else
         {
+            ret_val = image_list->priv->images->length > 1;
             iter->priv->link = image_list->priv->images->head;
             iter->priv->index = 0;
         }
 
         iter->priv->r_file = iter->priv->link->data;
         iter->priv->sticky = sticky;
-        ret_val = TRUE;
     }
 
     if (ret_val == TRUE)
@@ -1030,18 +1027,19 @@ iter_previous (
     {
         if (iter->priv->link->prev != NULL)
         {
+            ret_val = TRUE;
             iter->priv->link = iter->priv->link->prev;
             iter->priv->index--;
         }
         else
         {
+            ret_val = image_list->priv->images->length > 1;
             iter->priv->link = image_list->priv->images->tail;
             iter->priv->index = image_list->priv->images->length - 1;
         }
 
         iter->priv->r_file = iter->priv->link->data;
         iter->priv->sticky = sticky;
-        ret_val = TRUE;
     }
 
     if (ret_val == TRUE)