xfce-mirror/ristretto

commit a1f94324e687aa491f49929c70ea4cdaa02ae2d0

sort-sync: Infrastructure for integration with a file manager

src/preferences_dialog.c
@@ -18,6 +18,7 @@
  */
 
 #include "util.h"
+#include "file_manager_integration.h"
 #include "preferences_dialog.h"
 
 #include <libxfce4ui/libxfce4ui.h>
@@ -68,8 +69,15 @@ cb_choose_desktop_combo_box_changed (GtkComboBox *combo_box,
 static void
 cb_slideshow_timeout_value_changed (GtkRange *range,
                                     gpointer user_data);
+static void
+cb_check_file_manager_sort_sync_toggled (GtkToggleButton *button,
+                                         gpointer user_data);
+static void
+cb_check_file_manager_sort_sync_once_toggled (GtkToggleButton *button,
+                                              gpointer user_data);
 
-
+static void
+cb_update_file_manager_buttons (RsttoPreferencesDialog *dialog);
 
 struct _RsttoPreferencesDialogPrivate
 {
@@ -130,6 +138,8 @@ struct _RsttoPreferencesDialogPrivate
         GtkWidget *desktop_frame;
         GtkWidget *desktop_vbox;
         GtkWidget *choose_desktop_combo_box;
+        GtkWidget *check_file_manager_sort_sync;
+        GtkWidget *check_file_manager_sort_sync_once;
 
         GtkWidget *startup_frame;
         GtkWidget *startup_vbox;
@@ -198,6 +208,8 @@ rstto_preferences_dialog_init (RsttoPreferencesDialog *dialog)
     gboolean bool_show_clock;
     gboolean bool_limit_quality;
     gboolean bool_enable_smoothing;
+    gboolean bool_file_manager_sort_sync;
+    gboolean bool_file_manager_sort_sync_once;
     gchar *str_desktop_type = NULL;
 
     GdkRGBA *bgcolor;
@@ -239,6 +251,8 @@ rstto_preferences_dialog_init (RsttoPreferencesDialog *dialog)
                   "show-clock", &bool_show_clock,
                   "limit-quality", &bool_limit_quality,
                   "enable-smoothing", &bool_enable_smoothing,
+                  "file-manager-sort-sync", &bool_file_manager_sort_sync,
+                  "file-manager-sort-sync-once", &bool_file_manager_sort_sync_once,
                   NULL);
 
     /*
@@ -504,6 +518,29 @@ rstto_preferences_dialog_init (RsttoPreferencesDialog *dialog)
     g_signal_connect (dialog->priv->behaviour_tab.choose_desktop_combo_box, "changed",
                       G_CALLBACK (cb_choose_desktop_combo_box_changed), dialog);
 
+    /* File manager sync checkbox */
+    dialog->priv->behaviour_tab.check_file_manager_sort_sync = gtk_check_button_new_with_label (_("Synchronize sorting with file manager"));
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (dialog->priv->behaviour_tab.check_file_manager_sort_sync),
+                                  bool_file_manager_sort_sync);
+    gtk_box_pack_start (GTK_BOX (dialog->priv->behaviour_tab.desktop_vbox),
+                        dialog->priv->behaviour_tab.check_file_manager_sort_sync,
+                        FALSE, FALSE, 0);
+    g_signal_connect (dialog->priv->behaviour_tab.check_file_manager_sort_sync, "toggled",
+                      G_CALLBACK (cb_check_file_manager_sort_sync_toggled), dialog);
+
+    /* File manager sync once checkbox */
+    dialog->priv->behaviour_tab.check_file_manager_sort_sync_once = gtk_check_button_new_with_label (_("Synchronize only once when opening a directory"));
+    g_object_bind_property (dialog->priv->behaviour_tab.check_file_manager_sort_sync, "active",
+                            dialog->priv->behaviour_tab.check_file_manager_sort_sync_once, "visible",
+                            G_BINDING_SYNC_CREATE);
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (dialog->priv->behaviour_tab.check_file_manager_sort_sync_once),
+                                  bool_file_manager_sort_sync_once);
+    gtk_box_pack_start (GTK_BOX (dialog->priv->behaviour_tab.desktop_vbox),
+                        dialog->priv->behaviour_tab.check_file_manager_sort_sync_once,
+                        FALSE, FALSE, 0);
+    g_signal_connect (dialog->priv->behaviour_tab.check_file_manager_sort_sync_once, "toggled",
+                      G_CALLBACK (cb_check_file_manager_sort_sync_once_toggled), dialog);
+
     /* Increase left and top margins for the tabs' contents */
     n_pages = gtk_notebook_get_n_pages (GTK_NOTEBOOK (notebook));
     for (i = 0; i < n_pages; ++i)
@@ -515,6 +552,10 @@ rstto_preferences_dialog_init (RsttoPreferencesDialog *dialog)
 
     gtk_container_add (GTK_CONTAINER (gtk_dialog_get_content_area (GTK_DIALOG (dialog))), notebook);
     gtk_widget_show_all (notebook);
+
+    cb_update_file_manager_buttons (dialog);
+    g_signal_connect_swapped (dialog->priv->settings, "notify::desktop-type",
+                              G_CALLBACK (cb_update_file_manager_buttons), dialog);
 
     /* Window should not be resizable */
     gtk_window_set_resizable (GTK_WINDOW (dialog), FALSE);
@@ -538,6 +579,7 @@ rstto_preferences_dialog_finalize (GObject *object)
 
     if (dialog->priv->settings)
     {
+        g_signal_handlers_disconnect_by_data (dialog->priv->settings, dialog);
         g_object_unref (dialog->priv->settings);
         dialog->priv->settings = NULL;
     }
@@ -712,6 +754,28 @@ cb_slideshow_timeout_value_changed (GtkRange *range,
     /* Code Section */
 
     rstto_settings_set_uint_property (dialog->priv->settings, "slideshow-timeout", slideshow_timeout);
+}
+
+static void
+cb_check_file_manager_sort_sync_toggled (GtkToggleButton *button,
+                                         gpointer user_data)
+{
+    RsttoPreferencesDialog *dialog = user_data;
+
+    rstto_settings_set_boolean_property (dialog->priv->settings,
+                                         "file-manager-sort-sync",
+                                         gtk_toggle_button_get_active (button));
+}
+
+static void
+cb_check_file_manager_sort_sync_once_toggled (GtkToggleButton *button,
+                                              gpointer user_data)
+{
+    RsttoPreferencesDialog *dialog = user_data;
+
+    rstto_settings_set_boolean_property (dialog->priv->settings,
+                                         "file-manager-sort-sync-once",
+                                         gtk_toggle_button_get_active (button));
 }
 
 /**
@@ -1042,5 +1106,21 @@ cb_choose_desktop_combo_box_changed (GtkComboBox *combo_box,
         case DESKTOP_TYPE_GNOME:
             rstto_settings_set_string_property (dialog->priv->settings, "desktop-type", "gnome");
             break;
+    }
+}
+
+static void
+cb_update_file_manager_buttons (RsttoPreferencesDialog *dialog)
+{
+    gchar *desktop_type = rstto_settings_get_string_property (dialog->priv->settings, "desktop-type");
+    if (rstto_file_manager_integration_is_supported_for (rstto_desktop_environment_from_name (desktop_type)))
+    {
+        gtk_widget_show (dialog->priv->behaviour_tab.check_file_manager_sort_sync);
+        gtk_widget_show (dialog->priv->behaviour_tab.check_file_manager_sort_sync_once);
+    }
+    else
+    {
+        gtk_widget_hide (dialog->priv->behaviour_tab.check_file_manager_sort_sync);
+        gtk_widget_hide (dialog->priv->behaviour_tab.check_file_manager_sort_sync_once);
     }
 }

src/file_manager_integration.c
@@ -1,0 +1,75 @@
+/*
+ *  Copyright (c) Dmitry Petrachkov 2025 <dmitry-petrachkov@outlook.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ *  02110-1301, USA.
+ */
+
+#include "file_manager_integration.h"
+#include "file_manager_integration_thunar.h"
+#include "settings.h"
+
+G_DEFINE_INTERFACE (RsttoFileManagerIntegration, rstto_file_manager_integration, G_TYPE_OBJECT)
+
+static void
+rstto_file_manager_integration_default_init (RsttoFileManagerIntegrationInterface *iface)
+{
+    g_object_interface_install_property (iface,
+                                         g_param_spec_object ("directory",
+                                                              NULL,
+                                                              NULL,
+                                                              G_TYPE_FILE,
+                                                              G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+
+    g_object_interface_install_property (iface,
+                                         g_param_spec_int ("sort-type",
+                                                           NULL,
+                                                           NULL,
+                                                           -1, SORT_TYPE_COUNT,
+                                                           -1,
+                                                           G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
+
+
+    g_object_interface_install_property (iface,
+                                         g_param_spec_int ("sort-order",
+                                                           NULL,
+                                                           NULL,
+                                                           -1, SORT_ORDER_COUNT,
+                                                           -1,
+                                                           G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
+}
+
+void
+rstto_file_manager_integration_set_directory (RsttoFileManagerIntegration *self,
+                                              GFile *directory)
+{
+    g_object_set (self, "directory", directory, NULL);
+}
+
+gboolean
+rstto_file_manager_integration_is_supported_for (RsttoDesktopEnvironment desktop_env)
+{
+    switch (desktop_env)
+    {
+        default:
+            return FALSE;
+    }
+}
+
+RsttoFileManagerIntegration *
+rstto_file_manager_integration_factory_create (RsttoDesktopEnvironment desktop_env)
+{
+    return NULL;
+}

src/file_manager_integration.h
@@ -1,0 +1,50 @@
+/*
+ *  Copyright (c) Dmitry Petrachkov 2025 <dmitry-petrachkov@outlook.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ *  02110-1301, USA.
+ */
+
+#ifndef __RISTRETTO_FILE_MANAGER_INTEGRATION_H__
+#define __RISTRETTO_FILE_MANAGER_INTEGRATION_H__
+
+#include "settings.h"
+
+#include <gio/gio.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define RSTTO_TYPE_FILE_MANAGER_INTEGRATION (rstto_file_manager_integration_get_type ())
+G_DECLARE_INTERFACE (RsttoFileManagerIntegration, rstto_file_manager_integration, RSTTO, FILE_MANAGER_INTEGRATION, GObject)
+
+struct _RsttoFileManagerIntegrationInterface
+{
+    GTypeInterface parent;
+};
+
+void
+rstto_file_manager_integration_set_directory (RsttoFileManagerIntegration *self,
+                                              GFile *directory);
+
+gboolean
+rstto_file_manager_integration_is_supported_for (RsttoDesktopEnvironment desktop_env);
+
+RsttoFileManagerIntegration *
+rstto_file_manager_integration_factory_create (RsttoDesktopEnvironment desktop_env);
+
+G_END_DECLS
+
+#endif /* __RISTRETTO_FILE_MANAGER_INTEGRATION_H__ */

src/image_list.c
@@ -191,6 +191,7 @@ struct _RsttoImageListPrivate
 
     GFileMonitor *dir_monitor;
     GList *image_monitors;
+    GFile *directory;
 
     GQueue *images;
     GSList *iterators;
@@ -310,6 +311,8 @@ rstto_image_list_finalize (GObject *object)
             g_queue_free_full (image_list->priv->images, g_object_unref);
             image_list->priv->images = NULL;
         }
+
+        g_clear_object (&image_list->priv->directory);
     }
 
     G_OBJECT_CLASS (rstto_image_list_parent_class)->finalize (object);
@@ -491,6 +494,8 @@ rstto_image_list_remove_all (RsttoImageList *image_list)
 
     for (iter = image_list->priv->iterators; iter != NULL; iter = iter->next)
         iter_set_position (iter->data, -1, FALSE);
+
+    g_clear_object (&image_list->priv->directory);
 
     g_signal_emit (image_list,
                    rstto_image_list_signals[RSTTO_IMAGE_LIST_SIGNAL_REMOVE_ALL],
@@ -686,11 +691,21 @@ rstto_image_list_set_directory (RsttoImageList *image_list,
      */
     image_list->priv->is_busy = TRUE;
     rstto_object_set_data (dir, "loaded-file", file);
+
+    g_clear_object (&image_list->priv->directory);
+    image_list->priv->directory = g_object_ref (dir);
+
     g_file_enumerate_children_async (dir, "standard::name,standard::fast-content-type",
                                      G_FILE_QUERY_INFO_NONE, G_PRIORITY_DEFAULT, NULL,
                                      rstto_image_list_set_directory_enumerate_finish, image_list);
 
     return TRUE;
+}
+
+GFile *
+rstto_image_list_get_directory (RsttoImageList *image_list)
+{
+    return image_list->priv->directory;
 }
 
 static void

src/image_list.h
@@ -74,6 +74,9 @@ rstto_image_list_set_directory (RsttoImageList *image_list,
                                 RsttoFile *file,
                                 GError **error);
 
+GFile *
+rstto_image_list_get_directory (RsttoImageList *image_list);
+
 gboolean
 rstto_image_list_is_busy (RsttoImageList *list);
 

src/meson.build
@@ -3,6 +3,8 @@ ristretto_sources = [
   'app_menu_item.h',
   'file.c',
   'file.h',
+  'file_manager_integration.h',
+  'file_manager_integration.c',
   'gnome_wallpaper_manager.c',
   'gnome_wallpaper_manager.h',
   'icon_bar.c',

src/settings.c
@@ -55,6 +55,8 @@ enum
     PROP_SORT_ORDER,
     PROP_THUMBNAIL_SIZE,
     PROP_DEFAULT_ZOOM,
+    PROP_FILE_MANAGER_SORT_SYNC,
+    PROP_FILE_MANAGER_SORT_SYNC_ONCE,
 };
 
 static RsttoSettings *settings_object;
@@ -108,6 +110,8 @@ struct _RsttoSettingsPrivate
     gboolean maximize_on_startup;
     RsttoThumbnailSize thumbnail_size;
     RsttoScale default_zoom;
+    gboolean file_manager_sort_sync;
+    gboolean file_manager_sort_sync_once;
 
     RsttoSortType sort_type;
     RsttoSortOrder sort_order;
@@ -213,6 +217,8 @@ rstto_settings_init (RsttoSettings *settings)
     xfconf_g_property_bind (settings->priv->channel, "/errors/missing-thumbnailer", G_TYPE_BOOLEAN, settings, "show-error-missing-thumbnailer");
     xfconf_g_property_bind (settings->priv->channel, "/desktop/type", G_TYPE_STRING, settings, "desktop-type");
     xfconf_g_property_bind (settings->priv->channel, "/image/default-zoom", G_TYPE_INT, settings, "default-zoom");
+    xfconf_g_property_bind (settings->priv->channel, "/window/file-manager-sort-sync", G_TYPE_BOOLEAN, settings, "file-manager-sort-sync");
+    xfconf_g_property_bind (settings->priv->channel, "/window/file-manager-sort-sync-once", G_TYPE_BOOLEAN, settings, "file-manager-sort-sync-once");
 }
 
 
@@ -381,6 +387,18 @@ rstto_settings_class_init (RsttoSettingsClass *klass)
                               RSTTO_SCALE_NONE, RSTTO_SCALE_REAL_SIZE, RSTTO_SCALE_NONE,
                               G_PARAM_READWRITE);
     g_object_class_install_property (object_class, PROP_DEFAULT_ZOOM, pspec);
+
+    pspec = g_param_spec_boolean ("file-manager-sort-sync",
+                                  "", "",
+                                  FALSE,
+                                  G_PARAM_READWRITE);
+    g_object_class_install_property (object_class, PROP_FILE_MANAGER_SORT_SYNC, pspec);
+
+    pspec = g_param_spec_boolean ("file-manager-sort-sync-once",
+                                  "", "",
+                                  FALSE,
+                                  G_PARAM_READWRITE);
+    g_object_class_install_property (object_class, PROP_FILE_MANAGER_SORT_SYNC_ONCE, pspec);
 }
 
 /**
@@ -618,6 +636,12 @@ rstto_settings_set_property (GObject *object,
         case PROP_DEFAULT_ZOOM:
             settings->priv->default_zoom = g_value_get_int (value);
             break;
+        case PROP_FILE_MANAGER_SORT_SYNC:
+            settings->priv->file_manager_sort_sync = g_value_get_boolean (value);
+            break;
+        case PROP_FILE_MANAGER_SORT_SYNC_ONCE:
+            settings->priv->file_manager_sort_sync_once = g_value_get_boolean (value);
+            break;
         default:
             break;
     }
@@ -710,6 +734,12 @@ rstto_settings_get_property (GObject *object,
             break;
         case PROP_DEFAULT_ZOOM:
             g_value_set_int (value, settings->priv->default_zoom);
+            break;
+        case PROP_FILE_MANAGER_SORT_SYNC:
+            g_value_set_boolean (value, settings->priv->file_manager_sort_sync);
+            break;
+        case PROP_FILE_MANAGER_SORT_SYNC_ONCE:
+            g_value_set_boolean (value, settings->priv->file_manager_sort_sync_once);
             break;
         default:
             break;
@@ -834,4 +864,38 @@ rstto_settings_get_boolean_property (RsttoSettings *settings,
     g_object_get (settings, property_name, &value, NULL);
 
     return value;
+}
+
+/* RsttoDesktopEnvironment */
+
+const gchar *
+rstto_desktop_environment_get_name (RsttoDesktopEnvironment desktop_env)
+{
+    switch (desktop_env)
+    {
+        case DESKTOP_ENVIRONMENT_XFCE:
+            return "xfce";
+
+        case DESKTOP_ENVIRONMENT_GNOME:
+            return "gnome";
+
+        case DESKTOP_ENVIRONMENT_NONE:
+        default:
+            return "none";
+    }
+}
+
+RsttoDesktopEnvironment
+rstto_desktop_environment_from_name (const gchar *name)
+{
+    if (name == NULL)
+        return DESKTOP_ENVIRONMENT_NONE;
+
+    if (g_strcmp0 (name, "xfce") == 0)
+        return DESKTOP_ENVIRONMENT_XFCE;
+
+    if (g_strcmp0 (name, "gnome") == 0)
+        return DESKTOP_ENVIRONMENT_GNOME;
+
+    return DESKTOP_ENVIRONMENT_NONE;
 }

commit 8d458879b9ae0a650aab51af8daccc90f426d0a9

preferences: Fix string leak in cb_update_file_manager_buttons
Fixes: a1f94324e

src/preferences_dialog.c
@@ -1123,4 +1123,5 @@ cb_update_file_manager_buttons (RsttoPreferencesDialog *dialog)
         gtk_widget_hide (dialog->priv->behaviour_tab.check_file_manager_sort_sync);
         gtk_widget_hide (dialog->priv->behaviour_tab.check_file_manager_sort_sync_once);
     }
+    g_free (desktop_type);
 }