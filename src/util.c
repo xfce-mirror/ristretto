608f2e77b5192179508ba3049e65b78d071a0649
Dmitry Petrachkov <dmitry-petrachkov@outlook.com>
2025-10-03 13:58:56 +0300

    ephemeral-file: Implementation

--- src/util.c
+++ src/util.c
@@ -215,3 +215,38 @@ rstto_util_get_thumbnail_n_pixels (RsttoThumbnailSize size)
 {
     return rstto_thumbnail_n_pixels[size];
 }
+
+
+
+gboolean
+rstto_util_sendfile (GOutputStream *out,
+                     GInputStream *in,
+                     GError **error)
+{
+    gsize buff_size = 1 * 1024 * 1024;
+    gchar *buff = g_malloc (buff_size);
+
+    gboolean status = FALSE;
+
+    while (TRUE)
+    {
+        gssize n_in = g_input_stream_read (in, buff, buff_size, NULL, error);
+        if (n_in == -1)
+        {
+            break;
+        }
+        else if (n_in == 0)
+        {
+            status = TRUE;
+            break;
+        }
+
+        gssize n_out = g_output_stream_write (out, buff, n_in, NULL, error);
+        if (n_out != n_in)
+            break;
+    }
+
+    g_free (buff);
+
+    return status;
+}
