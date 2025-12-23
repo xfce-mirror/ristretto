xfce-mirror/ristretto

commit c13670419830412925c5b7f27719d82da22083e8

Add a toggle to enable/disable smoothing
Smoothing is performed by default, however sometimes it is desirable to
see the actual pixels of the image or to prioritize performance over
rendering quality.

Closes: #75

src/image_viewer.c
@@ -184,6 +184,10 @@ cb_rstto_limit_quality_changed (GObject *settings,
                                 GParamSpec *pspec,
                                 gpointer user_data);
 static void
+cb_rstto_smoothing_changed (GObject *settings,
+                            GParamSpec *pspec,
+                            gpointer user_data);
+static void
 cb_rstto_bgcolor_changed (GObject *settings,
                           GParamSpec *pspec,
                           gpointer user_data);
@@ -240,6 +244,7 @@ struct _RsttoImageViewerPrivate
     GdkPixbuf                   *bg_icon;
 
     gboolean                     limit_quality;
+    gboolean                     enable_smoothing;
 
     GError                      *error;
 
@@ -353,6 +358,8 @@ rstto_image_viewer_init (RsttoImageViewer *viewer)
                       G_CALLBACK (cb_rstto_bgcolor_changed), viewer);
     g_signal_connect (viewer->priv->settings, "notify::limit-quality",
                       G_CALLBACK (cb_rstto_limit_quality_changed), viewer);
+    g_signal_connect (viewer->priv->settings, "notify::enable-smoothing",
+                      G_CALLBACK (cb_rstto_smoothing_changed), viewer);
     g_signal_connect (viewer->priv->settings, "notify::invert-zoom-direction",
                       G_CALLBACK (cb_rstto_zoom_direction_changed), viewer);
     g_signal_connect (viewer, "drag-data-received",
@@ -522,6 +529,8 @@ rstto_image_viewer_realize (GtkWidget *widget)
 
     g_object_get (viewer->priv->settings, "limit-quality",
                   &(viewer->priv->limit_quality), NULL);
+    g_object_get (viewer->priv->settings, "enable-smoothing",
+                  &(viewer->priv->enable_smoothing), NULL);
     g_object_get (viewer->priv->settings, "invert-zoom-direction",
                   &(viewer->priv->invert_zoom_direction), NULL);
 }
@@ -1124,6 +1133,8 @@ paint_image (GtkWidget *widget, cairo_t *ctx)
             break;
     }
 
+    cairo_pattern_set_filter (viewer->priv->pixbuf.pattern,
+        viewer->priv->enable_smoothing ? CAIRO_FILTER_BILINEAR : CAIRO_FILTER_NEAREST);
     cairo_scale (ctx, x_scale / viewer->priv->image_scale, y_scale / viewer->priv->image_scale);
     cairo_set_source (ctx, viewer->priv->pixbuf.pattern);
     cairo_paint (ctx);
@@ -2126,6 +2137,16 @@ cb_rstto_limit_quality_changed (GObject *settings, GParamSpec *pspec, gpointer u
 
     if (NULL != viewer->priv->file)
         rstto_image_viewer_load_image (viewer, viewer->priv->file, viewer->priv->scale);
+}
+
+static void
+cb_rstto_smoothing_changed (GObject *settings, GParamSpec *pspec, gpointer user_data)
+{
+    RsttoImageViewer *viewer = user_data;
+
+    g_object_get (viewer->priv->settings, "enable-smoothing", &(viewer->priv->enable_smoothing), NULL);
+
+    gtk_widget_queue_draw (GTK_WIDGET (viewer));
 }
 
 static void

src/preferences_dialog.c
@@ -54,6 +54,9 @@ static void
 cb_limit_quality_check_button_toggled (GtkToggleButton *button,
                                        gpointer user_data);
 static void
+cb_enable_smoothing_check_button_toggled (GtkToggleButton *button,
+                                          gpointer user_data);
+static void
 cb_wrap_images_check_button_toggled (GtkToggleButton *button,
                                      gpointer user_data);
 static void
@@ -85,6 +88,8 @@ struct _RsttoPreferencesDialogPrivate
 
         GtkWidget *quality_label;
         GtkWidget *quality_button;
+        GtkWidget *enable_smoothing_label;
+        GtkWidget *enable_smoothing_check_button;
     } display_tab;
 
     struct
@@ -193,6 +198,7 @@ rstto_preferences_dialog_init (RsttoPreferencesDialog *dialog)
     gboolean   bool_maximize_on_startup;
     gboolean   bool_show_clock;
     gboolean   bool_limit_quality;
+    gboolean   bool_enable_smoothing;
     gchar     *str_desktop_type = NULL;
 
     GdkRGBA   *bgcolor;
@@ -234,6 +240,7 @@ rstto_preferences_dialog_init (RsttoPreferencesDialog *dialog)
             "desktop-type", &str_desktop_type,
             "show-clock", &bool_show_clock,
             "limit-quality", &bool_limit_quality,
+            "enable-smoothing", &bool_enable_smoothing,
             NULL);
 
     /*
@@ -295,6 +302,20 @@ rstto_preferences_dialog_init (RsttoPreferencesDialog *dialog)
 
     g_signal_connect (dialog->priv->display_tab.quality_button, "toggled",
                       G_CALLBACK (cb_limit_quality_check_button_toggled), dialog);
+
+    dialog->priv->display_tab.enable_smoothing_label = gtk_label_new (
+            _("Smooth the image using bilinear interpolation, thus prioritizing rendering quality over performance."));
+    gtk_label_set_line_wrap (GTK_LABEL (dialog->priv->display_tab.enable_smoothing_label), TRUE);
+    gtk_label_set_max_width_chars (GTK_LABEL (dialog->priv->display_tab.enable_smoothing_label), MAX_WIDTH_CHARS);
+    gtk_label_set_xalign (GTK_LABEL (dialog->priv->display_tab.enable_smoothing_label), XALIGN);
+    gtk_label_set_yalign (GTK_LABEL (dialog->priv->display_tab.enable_smoothing_label), YALIGN);
+    dialog->priv->display_tab.enable_smoothing_check_button = gtk_check_button_new_with_label (_("Enable smoothing"));
+    gtk_container_add (GTK_CONTAINER (dialog->priv->display_tab.quality_vbox), dialog->priv->display_tab.enable_smoothing_label);
+    gtk_container_add (GTK_CONTAINER (dialog->priv->display_tab.quality_vbox), dialog->priv->display_tab.enable_smoothing_check_button);
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (dialog->priv->display_tab.enable_smoothing_check_button), bool_enable_smoothing);
+
+    g_signal_connect (dialog->priv->display_tab.enable_smoothing_check_button, "toggled",
+                      G_CALLBACK (cb_enable_smoothing_check_button_toggled), dialog);
 
     /*
      * Fullscreen tab
@@ -975,6 +996,47 @@ cb_limit_quality_check_button_toggled (
             dialog->priv->settings,
             "limit-quality",
             limit_quality);
+}
+
+/**
+ * cb_enable_smoothing_check_button_toggled:
+ * @button:    The check-button the user clicked.
+ * @user_data: The user-data provided when connecting the
+ *             callback-function, the preferences-dialog.
+ *
+ *
+ * This function is called when a user toggles the 'enable-smoothing'
+ * check-button. This function then sets the right property in the
+ * ristretto settings container.
+ *
+ * When this property is set, the image is smoothed using bilinear
+ * interpolation, thus prioritizing rendering quality over performance.
+ *
+ *
+ *   active = toggle_button_get_active ()
+ *
+ *   if (active) then
+ *
+ *       set_property ("enable-smoothing", TRUE);
+ *
+ *   else
+ *
+ *       set_property ("enable-smoothing", FALSE);
+ *
+ *   endif
+ */
+static void
+cb_enable_smoothing_check_button_toggled (
+        GtkToggleButton *button,
+        gpointer user_data)
+{
+    RsttoPreferencesDialog *dialog = user_data;
+    gboolean enable_smoothing = gtk_toggle_button_get_active (button);
+
+    rstto_settings_set_boolean_property (
+            dialog->priv->settings,
+            "enable-smoothing",
+            enable_smoothing);
 }
 
 /**

src/settings.c
@@ -35,6 +35,7 @@ enum
     PROP_SHOW_STATUSBAR,
     PROP_SHOW_CLOCK,
     PROP_LIMIT_QUALITY,
+    PROP_ENABLE_SMOOTHING,
     PROP_HIDE_THUMBNAILS_FULLSCREEN,
     PROP_HIDE_MOUSE_CURSOR_FULLSCREEN_TIMEOUT,
     PROP_WINDOW_WIDTH,
@@ -88,6 +89,7 @@ struct _RsttoSettingsPrivate
     gboolean  show_statusbar;
     gboolean  show_clock;
     gboolean  limit_quality;
+    gboolean  enable_smoothing;
     gboolean  hide_thumbnails_fullscreen;
     guint     hide_mouse_cursor_fullscreen_timeout;
     gchar    *navigationbar_position;
@@ -157,6 +159,7 @@ rstto_settings_init (RsttoSettings *settings)
     settings->priv->bgcolor = g_new0 (GdkRGBA, 1);
     settings->priv->bgcolor_fullscreen = g_new0 (GdkRGBA, 1);
     gdk_rgba_parse (settings->priv->bgcolor_fullscreen, "black"); // black by default
+    settings->priv->enable_smoothing = TRUE;
     settings->priv->navigationbar_position = g_strdup ("left");
     settings->priv->show_toolbar = TRUE;
     settings->priv->window_width = 600;
@@ -307,6 +310,13 @@ rstto_settings_init (RsttoSettings *settings)
             settings,
             "limit-quality");
 
+    xfconf_g_property_bind (
+            settings->priv->channel,
+            "/image/enable-smoothing",
+            G_TYPE_BOOLEAN,
+            settings,
+            "enable-smoothing");
+
     /* not Thunar-specific but let's keep the old name for backwards-compatibility */
     xfconf_g_property_bind (
             settings->priv->channel,
@@ -434,6 +444,17 @@ rstto_settings_class_init (RsttoSettingsClass *klass)
     g_object_class_install_property (
             object_class,
             PROP_LIMIT_QUALITY,
+            pspec);
+
+    pspec = g_param_spec_boolean (
+            "enable-smoothing",
+            "",
+            "",
+            TRUE,
+            G_PARAM_READWRITE);
+    g_object_class_install_property (
+            object_class,
+            PROP_ENABLE_SMOOTHING,
             pspec);
 
     pspec = g_param_spec_boolean (
@@ -785,6 +806,9 @@ rstto_settings_set_property (GObject      *object,
         case PROP_LIMIT_QUALITY:
             settings->priv->limit_quality = g_value_get_boolean (value);
             break;
+        case PROP_ENABLE_SMOOTHING:
+            settings->priv->enable_smoothing = g_value_get_boolean (value);
+            break;
         case PROP_HIDE_THUMBNAILS_FULLSCREEN:
             settings->priv->hide_thumbnails_fullscreen = g_value_get_boolean (value);
             break;
@@ -894,6 +918,9 @@ rstto_settings_get_property (GObject    *object,
             break;
         case PROP_LIMIT_QUALITY:
             g_value_set_boolean (value, settings->priv->limit_quality);
+            break;
+        case PROP_ENABLE_SMOOTHING:
+            g_value_set_boolean (value, settings->priv->enable_smoothing);
             break;
         case PROP_HIDE_THUMBNAILS_FULLSCREEN:
             g_value_set_boolean (value, settings->priv->hide_thumbnails_fullscreen);

commit bfc0e360f1304c7ab7cdf13dd618ca1268c63ecc

Use CAIRO_FILTER_GOOD instead of BILINEAR when smoothing is enabled
BILINEAR produces a degraded rendering in some cases (see the forum
thread below), while GOOD or BEST produces the correct rendering that
occurred before adding the rendering quality option. Given the cost
associated with BEST, GOOD seems to be the right choice, until this
option is perhaps expanded into a drop-down list.

Fixes: c13670419830412925c5b7f27719d82da22083e8
Related: https://forum.xfce.org/viewtopic.php?id=19017

src/image_viewer.c
@@ -1132,7 +1132,7 @@ paint_image (GtkWidget *widget,
     }
 
     cairo_pattern_set_filter (viewer->priv->pixbuf.pattern,
-                              viewer->priv->enable_smoothing ? CAIRO_FILTER_BILINEAR
+                              viewer->priv->enable_smoothing ? CAIRO_FILTER_GOOD
                                                              : CAIRO_FILTER_NEAREST);
     cairo_scale (ctx, x_scale / viewer->priv->quality_scale, y_scale / viewer->priv->quality_scale);
     cairo_set_source (ctx, viewer->priv->pixbuf.pattern);