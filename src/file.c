xfce-mirror/ristretto

commit 7937cad27ef2bebc25deffc508f3e19d30fc2bde

gobject-linter: Fix use_g_steal_pointer

src/image_viewer.c
@@ -1751,8 +1751,7 @@ cb_rstto_image_loader_closed_idle (gpointer data)
             gtk_widget_set_tooltip_text (widget, transaction->error->message);
         }
 
-        viewer->priv->error = transaction->error;
-        transaction->error = NULL;
+        g_steal_pointer (&transaction->error);
         viewer->priv->transaction = NULL;
 
         gdk_window_invalidate_rect (gtk_widget_get_window (widget), NULL, FALSE);

commit d9cc1bd5d3ab9203d2c6ce7e4b5893df91e46e2e

Fix previous commits
Fixes: 7937cad27ef2bebc25deffc508f3e19d30fc2bde
Fixes: 4ebf164a2337904a2689183760cdcbba5bc27909

src/file.c
@@ -167,18 +167,12 @@ rstto_file_finalize (GObject *object)
 
     for (i = 0; i < RSTTO_THUMBNAIL_FLAVOR_COUNT; ++i)
     {
-        if (r_file->priv->thumbnail_paths[i])
-        {
-            g_clear_pointer (&r_file->priv->thumbnail_paths[i], g_free);
-        }
+        g_clear_pointer (&r_file->priv->thumbnail_paths[i], g_free);
     }
 
     for (i = 0; i < RSTTO_THUMBNAIL_SIZE_COUNT; ++i)
     {
-        if (r_file->priv->pixbufs[i])
-        {
-            g_clear_object (&r_file->priv->pixbufs[i]);
-        }
+        g_clear_object (&r_file->priv->pixbufs[i]);
     }
 
     G_OBJECT_CLASS (rstto_file_parent_class)->finalize (object);

src/image_viewer.c
@@ -1751,7 +1751,7 @@ cb_rstto_image_loader_closed_idle (gpointer data)
             gtk_widget_set_tooltip_text (widget, transaction->error->message);
         }
 
-        g_steal_pointer (&transaction->error);
+        viewer->priv->error = g_steal_pointer (&transaction->error);
         viewer->priv->transaction = NULL;
 
         gdk_window_invalidate_rect (gtk_widget_get_window (widget), NULL, FALSE);